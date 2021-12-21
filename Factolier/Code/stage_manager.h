851b325d18bdfd853ad16b3684bda4a06b006e10
らくえもん <85974181+RaKuemonn@users.noreply.github.com>
2021-12-21 12:31:35 +0900

    ファイル追加

--- Factolier/Code/stage_manager.h
+++ Factolier/Code/stage_manager.h
@@ -1,7 +1,9 @@
 #pragma once
 
-
 #include <memory>
+#include <vector>
+class Stage;
+
 
 class Stage_Manager final
 {
@@ -17,4 +19,18 @@ public:
     }
 
     void render();
+
+    void spawn_register(std::unique_ptr<Stage>& stage);
+    void spawn_register(std::unique_ptr<Stage>&& stage);
+    void spawn_register(std::shared_ptr<Stage>& stage);
+    void remove_register(Stage* stage);
+
+    void all_clear();
+
+
+
+private:
+    std::vector<std::shared_ptr<Stage>> vec_stages = {};
+    std::vector<std::shared_ptr<Stage>> vec_registers = {};
+    std::vector<std::shared_ptr<Stage>> vec_removes = {};
 };
