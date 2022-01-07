4385e87e07ffb03570e5cadaf9c7e93fae0fcd33
sisyamo5050 <68419685+sisyamo5050@users.noreply.github.com>
2022-01-07 17:37:31 +0900

    ブレンドモードにアルファカバレッジマスク生成モードを追加しました

--- Factolier/Code/scene_title.cpp
+++ Factolier/Code/scene_title.cpp
@@ -30,7 +30,7 @@ void Scene_Title::initialize()
     DirectX::XMFLOAT3 target = { 0.0f, 0.0f, 0.0f };
     camera_controller->set_target(target);
 
-    test_model = std::make_unique<Model>(parent->model_manager()->load_model("./Data/nico.fbx"));
+    test_model = std::make_unique<Model>(parent->model_manager()->load_model("./Data/test_tree.fbx"));
     stage_model = std::make_unique<Model>(parent->model_manager()->load_model("./Data/stage_demo.fbx"));
     //test_model->append_animation("./Data/Animations/Idle.fbx");
     //test_model = std::make_unique<Model>(parent->model_manager()->load_model("./Data/green.fbx"));
@@ -48,7 +48,7 @@ void Scene_Title::update(float elapsed_time)
 {
     camera_controller->update(elapsed_time, parent->input_manager());
 
-    test_model->play_animation(elapsed_time, 1);
+    //test_model->play_animation(elapsed_time, 1);
 
 }
 
@@ -65,22 +65,46 @@ void Scene_Title::render(float elapsed_time)
 
     parent->state_manager()->setRS(RS::SOLID_BACK);
 
+
+    //s—ñ‚ðì¬
     DirectX::XMFLOAT4X4 world = {
         1.0f, 0.0f, 0.0f, 0.0f,
         0.0f, 1.0f, 0.0f, 0.0f,
         0.0f, 0.0f, 1.0f, 0.0f,
         0.0f, 0.0f, 0.0f, 1.0f
     };
-    DirectX::XMMATRIX S = DirectX::XMMatrixScaling(0.01f, 0.01f, 0.01f);
-    //DirectX::XMMATRIX S = DirectX::XMMatrixScaling(1.0f, 0.15f, 1.0f);
-    DirectX::XMMATRIX R = DirectX::XMMatrixRotationRollPitchYaw(DirectX::XMConvertToRadians(0), 0, 0);
-    DirectX::XMMATRIX T = DirectX::XMMatrixTranslation(0, 0, 0);
-    DirectX::XMMATRIX W = S * R * T;
 
-    XMFLOAT4X4 coordinate_system_transforms = { -1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1 };	// 0:RHS Y-UP
-    XMMATRIX CW = XMLoadFloat4x4(&coordinate_system_transforms);
-    W *= CW;
-    DirectX::XMStoreFloat4x4(&world, W);
+    DirectX::XMFLOAT4X4 world_stage = {
+            1.0f, 0.0f, 0.0f, 0.0f,
+            0.0f, 1.0f, 0.0f, 0.0f,
+            0.0f, 0.0f, 1.0f, 0.0f,
+            0.0f, 0.0f, 0.0f, 1.0f
+    };
+
+    {
+        DirectX::XMMATRIX S = DirectX::XMMatrixScaling(0.005f, 0.005f, 0.005f);
+        //DirectX::XMMATRIX S = DirectX::XMMatrixScaling(1.0f, 1.0f, 1.0f);
+        //DirectX::XMMATRIX S = DirectX::XMMatrixScaling(1.0f, 0.15f, 1.0f);
+        DirectX::XMMATRIX R = DirectX::XMMatrixRotationRollPitchYaw(DirectX::XMConvertToRadians(0), 0, DirectX::XMConvertToRadians(0));
+        DirectX::XMMATRIX T = DirectX::XMMatrixTranslation(0, 0, 0);
+        DirectX::XMMATRIX W = S * R * T;
+
+        XMFLOAT4X4 coordinate_system_transforms = { -1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1 };	// 0:RHS Y-UP
+        XMMATRIX CW = XMLoadFloat4x4(&coordinate_system_transforms);
+        W *= CW;
+        DirectX::XMStoreFloat4x4(&world, W);
+
+        
+        DirectX::XMMATRIX S_s = DirectX::XMMatrixScaling(0.1f, 0.1f, 0.1f);
+        //DirectX::XMMATRIX S = DirectX::XMMatrixScaling(1.0f, 0.15f, 1.0f);
+        DirectX::XMMATRIX R_s = DirectX::XMMatrixRotationRollPitchYaw(DirectX::XMConvertToRadians(0), 0, 0);
+        DirectX::XMMATRIX T_s = DirectX::XMMatrixTranslation(0, 0, 0);
+        DirectX::XMMATRIX W_s = S_s * R_s * T_s;
+
+        W_s *= CW;
+        DirectX::XMStoreFloat4x4(&world_stage, W_s);
+    }
+
 
 
     Shader* shader = nullptr;
@@ -133,7 +157,7 @@ void Scene_Title::render(float elapsed_time)
 
         shader->begin(parent->device_context(), elapsed_time * 0.1f);
 
-        stage_model->render(parent->device_context(), world, { 1.0f, 1.0f, 1.0f, 1.0f });
+        stage_model->render(parent->device_context(), world_stage, { 1.0f, 1.0f, 1.0f, 1.0f });
         test_model->render(parent->device_context(), world, { 1.0f, 1.0f, 1.0f, 1.0f });
 
         shader->end(parent->device_context());
