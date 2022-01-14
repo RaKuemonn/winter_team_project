3fe49b7a78931694bb6b7a13af0aa654d5b49fde
らくえもん <85974181+RaKuemonn@users.noreply.github.com>
2022-01-14 11:18:58 +0900

    ファイル追加

---
+++ Factolier/Code/data_filepaths.h
@@ -0,0 +1,18 @@
+#pragma once
+
+namespace Data_Paths
+{
+    namespace Sprites
+    {
+        inline constexpr char* const test = "aaa";
+    };
+
+
+    namespace Models
+    {
+        inline constexpr char* const test = "aaa";
+    };
+
+
+};
+
