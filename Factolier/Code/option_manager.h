84c52a1ea33953934ebb4fca952e22d593e864e2
sisyamo5050 <68419685+sisyamo5050@users.noreply.github.com>
2022-01-20 17:52:53 +0900

    option_manager追加

---
+++ Factolier/Code/option_manager.h
@@ -0,0 +1,19 @@
+#pragma once
+
+#include <d3d11.h>
+
+
+
+class Option_Manager
+{
+public:
+    Option_Manager(ID3D11Device* device, ID3D11DeviceContext* context);
+    ~Option_Manager() = default;
+
+    void update();
+    void render();
+
+private:
+    ID3D11DeviceContext* immediate_context;
+
+};
