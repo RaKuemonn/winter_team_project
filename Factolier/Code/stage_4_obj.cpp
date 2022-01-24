cf445a50905c360ab3703cbfed446637bbd6b06a
らくえもん <85974181+RaKuemonn@users.noreply.github.com>
2022-01-24 14:48:07 +0900

    stageファイル追加

---
+++ Factolier/Code/stage_4_obj.cpp
@@ -0,0 +1,22 @@
+
+#include "stage_4_obj.h"
+#include "scene_manager.h"
+#include "transform.h"
+#include "model_filepaths.h"
+
+Stage_4_Obj::Stage_4_Obj(Scene_Manager* ptr_scene_manager_)
+{
+    load_model(ptr_scene_manager_->model_manager()->load_model(Model_Paths::Stage::stage_4_obj, true));
+
+    get_transform()->set_position({ 0.0f,-10.0f,0.0f });
+    get_transform()->set_scale(stage::scale);
+    get_transform()->set_euler({ 0.0f, -3.14f, 0.0f });
+    get_transform()->Update();
+
+    set_is_raycast(false);
+}
+
+void Stage_4_Obj::update(const float elapsed_time)
+{
+
+}
