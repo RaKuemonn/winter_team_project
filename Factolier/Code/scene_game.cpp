c8cf74e69dd2807bf6acff1c0bf8ef51357587ff
sisyamo5050 <68419685+sisyamo5050@users.noreply.github.com>
2022-01-24 22:12:59 +0900

    なんかわからんけどプッシュ

--- Factolier/Code/scene_game.cpp
+++ Factolier/Code/scene_game.cpp
@@ -332,21 +332,21 @@ void Scene_Game::init_stage(const Stage_Select stage_)
     case Stage_Select::STAGE_1:
     {
         stage_spawner->set_stage_1();
-        sky_box = std::make_unique<Sky_Box>(parent->device(), L"./Data/Sky_Box/stage_4.dds");
+        sky_box = std::make_unique<Sky_Box>(parent->device(), L"./Data/Sky_Box/stage_1.dds");
         break;
     }
 
     case Stage_Select::STAGE_2:
     {
         stage_spawner->set_stage_2();
-        sky_box = std::make_unique<Sky_Box>(parent->device(), L"./Data/Sky_Box/stage_4.dds");
+        sky_box = std::make_unique<Sky_Box>(parent->device(), L"./Data/Sky_Box/stage_2.dds");
         break;
     }
 
     case Stage_Select::STAGE_3:
     {
         stage_spawner->set_stage_3();
-        sky_box = std::make_unique<Sky_Box>(parent->device(), L"./Data/Sky_Box/stage_4.dds");
+        sky_box = std::make_unique<Sky_Box>(parent->device(), L"./Data/Sky_Box/stage_3.dds");
         break;
     }
 
